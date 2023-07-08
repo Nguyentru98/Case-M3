67ff74ff1073b6856f301801caab8d72436cf7d7
Tru Nguyen <nguyentruhn@gmail.com>
2023-07-08 19:03:20 +0700

    Init project

---
+++ Node-M3-Router/src/router/router.js
@@ -0,0 +1,13 @@
+import fs from 'fs'
+import productRouter from "./productRouter.js";
+import userRouter from "./userRouter.js";
+import homeController from "../controller/homeController.js";
+import cartRouter from './cartRouter.js';
+
+let router = {
+    '/': homeController.showIndex,
+    '/err': homeController.showErr,
+}
+router = {...router, ...productRouter, ...cartRouter};
+router = {...router, ...userRouter};
+export default router;
---
+++ Node-M3-Router/src/service/productService.js
@@ -0,0 +1,76 @@
+import connection from "../connection.js";
+
+
+class ProductService {
+    constructor() {
+        connection.connecting();
+    }
+
+    findAll() {
+        return new Promise((resolve, reject) => {
+            connection.getConnection().query('select * from products', (err, products) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    console.log(products)
+                    resolve(products)
+                }
+            })
+        })
+    }
+
+    save(product) {
+        return new Promise((resolve, reject) => {
+            connection.getConnection().query(`INSERT INTO products (name,price , quantity, image) VALUES ('${product.name}', ${product.quantity}, ${product.price},'${product.image}');`, (err, data) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    console.log('tao thanh cong')       
+                    resolve(data)
+                }
+            })
+        })
+
+    }
+    update(product) {
+            return new Promise((resolve, reject) => {
+                connection.getConnection().query(`UPDATE product SET name = '${product.name}', price = ${product.price}, quantity = '${product.quantity}', image = '${product.image}' WHERE id = ${product.id}`, (err, products) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        console.log(`Sửa thành công!`)
+                        resolve(products)
+                    }
+                })          
+            })
+        }
+
+    deleteId(id) {
+        return new Promise((resolve, reject) => {
+            connection.getConnection().query(`DELETE FROM product WHERE id = ${id} `, (err, products) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    console.log(`xóa thành công!`)
+                    resolve(products)
+                }
+            })          
+        })
+    }
+    
+    findById(id) {
+        return new Promise((resolve, reject) => {
+            connection.getConnection().query(`select * from product where id = ${id}`, (err, products) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(products[0])
+                }
+            })
+        })
+        
+    }
+}
+
+
+export default new ProductService();
